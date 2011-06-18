66f84bdaca57fb21839ee7034882539dac413a90
jean <jean@localhost>
2011-06-18 15:49:45 +0200

    cleanup kernel patches

--- cyanide_bootramdisk/patch.c
+++ cyanide_bootramdisk/patch.c
@@ -24,56 +24,40 @@
 #include "device.h"
 #include "commands.h"
 
+typedef struct kpatch{
+	char* description;
+	char* search;
+	char* replace;
+	int length;
+	int found;
+} kpatch;
 
-int patch_kernel(unsigned char* address, unsigned int size) {
-	unsigned int target = 0;
-	/*
-	CSED: 00 00 00 00 01 00 00 00 80 00 00 00 00 00 00 00 => 01 00 00 00 01 00 00 00 80 00 00 00 00 00 00 00 ; armv6 & armv7
+kpatch patchs[]= {
+	{"CSED", "\x00\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00", "\x01\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00", 16,0},
+	{"AMFI", "\x01\xD1\x01\x30\x04\xE0\x02\xDB", "\x00\x20\x01\x30\x04\xE0\x02\xDB", 8,0},
+	{"UID", "\x56\xD0\x40\xF6", "\x00\x00\x40\xF6", 4,0}
+};
+#define NPATCHS 	sizeof(patchs)/sizeof(kpatch)
 
-	AMFI: 00 B1 00 24 20 46 90 BD  +  0 => 00 B1 01 24 20 46 90 BD ; armv7
-	      0E 00 A0 E1 01 10 84 E2  + 20 => 00 00 00 00 ; armv6
-
-	TFP0: 85 68 00 23 02 93 01 93  +  8 => 0B E0 C0 46 ; armv7
-	      85 68 .. 93 .. 93 00 2c          0B D1
-	      85 68 02 93 01 93 00 2C  +  8 => 0E 93 BD 93 ; armv6
-	*/
-	unsigned int i = 0;
-	for(i = 0; i < size; i++) {
-        if(!memcmp(&address[i], "\x56\xD0\x40\xF6", 4)) {
-			target = i;
-			memcpy(&address[target], "\x00\x00\x40\xF6", 4);
-			continue;
+int patch_kernel(unsigned char* kaddr, unsigned int size) {
+	unsigned int j;
+	unsigned char* end = kaddr + size;
+	
+	while(kaddr < end)
+	{
+		for(j = 0; j < NPATCHS; j++)
+		{
+			if (patchs[j].found)
+				continue;
+			
+			if(!memcmp(patchs[j].search, kaddr, patchs[j].length))
+			{
+				memcpy(kaddr, patchs[j].replace, patchs[j].length);
+				patchs[j].found = 1;
+			}
 		}
-		//4.3.1 AMFI
-		if(!memcmp(&address[i], "\x01\xD1\x01\x30\x04\xE0\x02\xDB", 8)) {
-			target = i + 0;
-			memcpy((char*) &address[target], "\x00\x20\x01\x30\x04\xE0\x02\xDB", 8);
-			continue;
-		}
-		/*
-		 * Patch 1
-		 */
-		if(!memcmp(&address[i], "\x00\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00", 16)) {
-			target = i + 0;
-			memcpy(&address[target], "\x01\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00", 16);
-			continue;
-		}
-
-		/*
-		 * Patch 2
-		 */
-		if(!memcmp(&address[i], "\x00\xB1\x00\x24\x20\x46\x90\xBD", 8)) {
-			target = i + 0;
-			memcpy((char*) &address[target], "\x00\xB1\x01\x24\x20\x46\x90\xBD", 8);
-			continue;
-		}
-		if(!memcmp(&address[i], "\x00\x00\x50\xE3\x00\x00\x00\x0A\x00\x40\xA0\xE3\x04\x00\xA0\xE1", 16)) {
-			target = i + 8;
-			memcpy((char*) &address[target], "\x01\x40\xA0\xE3", 4);
-			continue;
-		}
-
-    }
+		kaddr++;
+	}
 	return 0;
 }
 
