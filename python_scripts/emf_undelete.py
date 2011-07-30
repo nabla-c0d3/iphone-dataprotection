955dc596afdcfd4672f262b9fd10f416747c549e
jean <jean@localhost>
2011-07-30 14:39:14 +0200

    poc code for recovering deleted files using the journal see http://www.dfrws.org/2008/proceedings/p76-burghardt.pdf

---
+++ python_scripts/emf_undelete.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from hfs.emf import EMFVolume
+from hfs.journal import do_emf_carving
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Usage: emf_undelete.py disk_image.bin"
+        sys.exit(0)
+    filename = sys.argv[1]
+    volume = EMFVolume(filename)
+    outdir = os.path.dirname(filename) + "/" + volume.volumeID().encode("hex") + "_" + os.path.basename(filename)
+    carveokdir = outdir + "/undelete/"
+    carvenokdir = outdir + "/junk/"
+    try:
+        os.makedirs(carveokdir)
+        os.makedirs(carvenokdir)
+    except:
+        pass
+    
+    do_emf_carving(volume, carveokdir, carvenokdir)
