d09a5662e2458072621ae79b83c6b8e0c9f6bd89
jean <jean@localhost>
2011-11-30 20:16:36 +0100

    Added iOS 4 kernel patches + NAND epoch fixer (issue 19) Use the iOS 4 IPSW matching the *installed* iOS version to generate a special patched kernel. Boot it using redsn0w. Wait for the "[FTL:MSG] Read back Signature OK" message to show up then reboot manually. The device should now boot without the "NAND format invalid" error in recovery mode.

--- python_scripts/kernel_patcher.py
+++ python_scripts/kernel_patcher.py
@@ -4,6 +4,7 @@ import plistlib
 import zipfile
 import struct
 import sys
+from optparse import OptionParser
 from Crypto.Cipher import AES
 from util.lzss import decompress_lzss
 
@@ -22,8 +23,23 @@ patchs_ios5 = {
     "AMFI" : (h("D0 47 01 21 40 B1 13 35"), h("00 20 01 21 40 B1 13 35")),
     "_PE_i_can_has_debugger" : (h("38 B1 05 49 09 68 00 29"), h("01 20 70 47 09 68 00 29")),
     "IOAESAccelerator enable UID" : (h("67 D0 40 F6"), h("00 20 40 F6")),
-    #not stritcly required, usefull for testing
+    #not stritcly required, useful for testing
     "getxattr system": ("com.apple.system.\x00", "com.apple.aaaaaa.\x00"),
+}
+
+patchs_ios4 = {
+    "NAND_epoch" : ("\x90\x47\x83\x45", "\x90\x47\x00\x20"),
+    "CSED" : ("\x00\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00", "\x01\x00\x00\x00\x01\x00\x00\x00\x80\x00\x00\x00\x00\x00\x00\x00"),
+    "AMFI" : ("\x01\xD1\x01\x30\x04\xE0\x02\xDB", "\x00\x20\x01\x30\x04\xE0\x02\xDB"),
+    "_PE_i_can_has_debugger" : (h("48 B1 06 4A 13 68 13 B9"), h("01 20 70 47 13 68 13 B9")),
+    "IOAESAccelerator enable UID" : ("\x56\xD0\x40\xF6", "\x00\x00\x40\xF6"),
+    "getxattr system": ("com.apple.system.\x00", "com.apple.aaaaaa.\x00"),
+}
+
+patchs_ios4_fixnand = {
+    "Please reboot => jump to prepare signature": (h("B0 47 DF F8 E8 04 F3 E1"), h("B0 47 DF F8 E8 04 1D E0")),
+    "prepare signature => jump to write signature": (h("10 43 18 60 DF F8 AC 04"), h("10 43 18 60 05 E1 00 20")),
+    "check write ok => infinite loop" : (h("A3 48 B0 47 01 24"), h("A3 48 B0 47 FE E7"))
 }
 
 #grab keys from redsn0w Keys.plist
@@ -46,7 +62,7 @@ class IPSWkeys(object):
         if not self.keys.has_key(filename):
             return None, None
         k = self.keys[filename]
-        return k["Key"], k["IV"]
+        return k.get("Key",""), k.get("IV","")
     
 def decryptImg3(blob, key, iv):
     assert blob[:4] == "3gmI", "Img3 magic tag"
@@ -62,7 +78,7 @@ def decryptImg3(blob, key, iv):
     return AES.new(key, AES.MODE_CBC, iv).decrypt(data)[:real_size]
 
 
-def main(ipswname):
+def main(ipswname, options):
     ipsw = zipfile.ZipFile(ipswname)
     manifest = plistlib.readPlistFromString(ipsw.read("BuildManifest.plist"))
     kernelname = manifest["BuildIdentities"][0]["Manifest"]["KernelCache"]["Info"]["Path"]
@@ -83,9 +99,22 @@ def main(ipswname):
     kernel = decompress_lzss(kernel)
     assert kernel.startswith("\xCE\xFA\xED\xFE"), "Decompressed kernelcache does not start with 0xFEEDFACE"
     
-    for p in patchs_ios5:
+    patchs = patchs_ios5
+    if manifest["ProductVersion"].startswith("4."):
+        print "Using iOS 4 kernel patches"
+        patchs = patchs_ios4
+    
+    if options.fixnand:
+        if patchs != patchs_ios4:
+            print "FAIL : use --fixnand with iOS 4.x IPSW"
+            return
+        patchs.update(patchs_ios4_fixnand)
+        kernelname = "fix_nand_" + kernelname
+        print "WARNING : only use this kernel to fix NAND epoch brick"
+    
+    for p in patchs:
         print "Doing %s patch" % p
-        s, r = patchs_ios5[p]
+        s, r = patchs[p]
         c = kernel.count(s)
         if c != 1:
             print "=> FAIL, count=%d, do not boot that kernel it wont work" % c
@@ -134,7 +163,13 @@ echo "%s"
     
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "Usage: %s IPSW" % __file__
-        sys.exit(-1)
-    main(sys.argv[1])
+    parser = OptionParser(usage="%prog [options] IPSW")
+    parser.add_option("-f", "--fixnand",
+                      action="store_true", dest="fixnand", default=False,
+                      help="Apply NAND epoch fix kernel patches")
+    
+    (options, args) = parser.parse_args()
+    if len(args) < 1:
+        parser.print_help()
+    else:
+        main(args[0], options)
